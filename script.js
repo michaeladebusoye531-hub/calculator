2040a421819b05bd17c21f493269b76fc8e53bc8
michaeladebusoye531-hub <michaeladebusoye531@gmail.com>
2026-06-22 16:05:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// CALCULATOR SCRIPT    
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function appendOperator(input) {
+    appendToDisplay(input);
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {   
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
